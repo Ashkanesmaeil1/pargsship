6a3cba87188bd868970e7c0fc307ca6d690db77f
Ashkan1279 <4shkanesmaeili@gmail.com>
2025-11-08 12:38:22 +0330

    update

--- main.js
+++ main.js
@@ -44,3 +44,55 @@ function closeModal() {
 document.addEventListener('keydown', (e) => {
     if (e.key === 'Escape') closeModal();
 });
+
+// انتخاب تصویر اصلی و تمام تصاویر گالری
+// const mainImage = document.querySelector('.product-main-image img');
+// const galleryImages = Array.from(document.querySelectorAll('.gallery-item img'));
+
+// let currentIndex = 0;
+// const intervalTime = 5000; // ۳ ثانیه
+
+// function changeImage() {
+//   // اضافه کردن افکت fade-out
+//   mainImage.classList.add('fade-out');
+
+//   setTimeout(() => {
+//     // تغییر عکس بعد از محو شدن
+//     mainImage.src = galleryImages[currentIndex].src;
+//     mainImage.alt = galleryImages[currentIndex].alt;
+
+//     // حذف fade-out تا دوباره ظاهر شود
+//     mainImage.classList.remove('fade-out');
+
+//     // رفتن به عکس بعدی
+//     currentIndex = (currentIndex + 1) % galleryImages.length;
+//   }, 1000); // باید با زمان transition در CSS یکی باشد (۱ ثانیه)
+// }
+
+// // شروع اتوماتیک چرخش تصاویر
+// setInterval(changeImage, intervalTime);
+
+const mainImage = document.querySelector('.product-main-image img');
+  const galleryImages = Array.from(document.querySelectorAll('.gallery-item img'));
+  const bgWrapper = document.querySelector('.bg-wrapper');
+
+  let currentIndex = 0;
+  const intervalTime = 8000;
+
+  function changeImage() {
+    mainImage.classList.add('fade-out');
+
+    setTimeout(() => {
+      const newSrc = galleryImages[currentIndex].src;
+
+      mainImage.src = newSrc;
+      mainImage.alt = galleryImages[currentIndex].alt;
+
+      bgWrapper.style.backgroundImage = `url('${newSrc}')`;
+
+      mainImage.classList.remove('fade-out');
+      currentIndex = (currentIndex + 1) % galleryImages.length;
+    }, 1000);
+  }
+
+  setInterval(changeImage, intervalTime);
